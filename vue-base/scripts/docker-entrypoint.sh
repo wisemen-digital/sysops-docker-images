0965452f0e8d552ec6ae4a42616d82e61f44bd69
David Jennes <david.jennes@wisemen.digital>
2024-03-19 18:41:42 +0100

    Vue: move working directory to "/app/www"

--- vue-base/scripts/docker-entrypoint.sh
+++ vue-base/scripts/docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ -f $ENV_EXAMPLE_PATH ]; then
   echo "Found example env at '$ENV_EXAMPLE_PATH', applying to codebase…"
   import-meta-env \
     --example $ENV_EXAMPLE_PATH \
-    --path '/usr/share/nginx/html/**/*' \
+    --path '/app/www/**/*' \
     --disposable || exit 1
 else
   echo 'No example env file found, skipping env import…'
