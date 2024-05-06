0b50e9a1d0746942211b4fc536f7caf7afc0ee8b
Bert Bijnens <bertbijnens@MacBook-Pro-van-Bert.local>
2024-05-07 01:08:25 +0200

    Duplicate php-base into octane-base for easy comparison

---
+++ octane-base/README.md
@@ -0,0 +1,29 @@
+# Base PHP Docker
+
+🐳 Generic docker image for PHP (Laravel) Applications.
+
+Available images (one for each PHP version):
+- `8.1`
+- `8.2`
+- `8.3`
+
+## Commands
+
+Docker will default to the `serve` command.
+
+- `serve`: starts all services, such as `nginx` and `php-fpm`.
+- `scheduler`: process scheduled tasks (cron jobs).
+- `queue`: process queued jobs. You can provide a specific queue with `--queue=my_queue`.
+- `init`: initialize the project, such as executing migrations.
+- `artisan`: helper to execute artisan commands.
+- fallback: execute the provided command.
+
+## Info
+
+Serves content on port `8080`.
+
+## Configuration
+
+You can configure the max. allowed uploads via these 2 variables:
+- `SERVER_POST_MAX_SIZE`: max post size (in nginx and PHP)
+- `SERVER_POST_MAX_FILESIZE`: max upload file size (in PHP)
---
+++ octane-base/scripts/docker-entrypoint.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+
+## Bootstrap
+
+
+# Apply max post envs
+echo "client_max_body_size ${SERVER_POST_MAX_SIZE:-8M};" > /etc/nginx/site-mods-available.d/max-upload.conf
+for php_version in /etc/php*; do
+  echo "php_admin_value[post_max_size] = ${SERVER_POST_MAX_SIZE:-8M}" >> $php_version/php-fpm.d/www.conf
+  echo "php_admin_value[upload_max_filesize] = ${SERVER_POST_MAX_FILESIZE:-2M}" >> $php_version/php-fpm.d/www.conf
+done
+
+# Cache laravel config
+if [ -f '/app/www/artisan' ]; then
+  php artisan config:cache
+fi
+
+
+## Commands ##
+
+
+# Helper to serve all services
+if [ "$1" = 'serve' ]; then
+  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
+# Helper to run cron jobs
+elif [ "$1" = 'scheduler' ]; then 
+  while [ true ]; do
+    php artisan schedule:run --verbose --no-interaction &
+    sleep 60
+  done
+
+# Helper to run queue jobs
+elif [ "$1" = 'queue' ]; then 
+  shift 1;
+  exec php artisan queue:work --verbose --tries=3 --timeout=60 --rest=0.5 --sleep=3 --max-jobs=1000 --max-time=3600  "$@"
+
+# Init script, for use in initContainers (for example)
+elif [ "$1" = 'init' ]; then 
+  exec php artisan migrate --isolated
+
+# Helper to run artisan commands
+elif [ "$1" = 'artisan' ]; then
+  shift 1;
+  exec php artisan "$@"
+
+# Fallback: just execute given command
+else
+  exec "$@"
+fi
