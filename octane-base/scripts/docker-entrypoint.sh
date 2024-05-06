72c0a5fff3a8cac91e1e6c5a68a83a5995398e35
Bert Bijnens <bertbijnens@MacBook-Pro-van-Bert.local>
2024-05-07 01:09:05 +0200

    Add octane-base Dockerfile, config and scripts

--- octane-base/scripts/docker-entrypoint.sh
+++ octane-base/scripts/docker-entrypoint.sh
@@ -5,14 +5,6 @@ set -euo pipefail
 
 ## Bootstrap
 
-
-# Apply max post envs
-echo "client_max_body_size ${SERVER_POST_MAX_SIZE:-8M};" > /etc/nginx/site-mods-available.d/max-upload.conf
-for php_version in /etc/php*; do
-  echo "php_admin_value[post_max_size] = ${SERVER_POST_MAX_SIZE:-8M}" >> $php_version/php-fpm.d/www.conf
-  echo "php_admin_value[upload_max_filesize] = ${SERVER_POST_MAX_FILESIZE:-2M}" >> $php_version/php-fpm.d/www.conf
-done
-
 # Cache laravel config
 if [ -f '/app/www/artisan' ]; then
   php artisan config:cache
