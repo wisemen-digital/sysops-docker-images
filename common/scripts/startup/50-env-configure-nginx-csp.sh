4382b4fc22e88780d862aa42ad52319778905b0c
David Jennes <david.jennes@wisemen.digital>
2025-03-01 00:22:59 +0100

    Nginx: provide mechanism to switch CSP mode

--- common/scripts/startup/50-env-configure-nginx-csp.sh
+++ common/scripts/startup/50-env-configure-nginx-csp.sh
@@ -5,14 +5,26 @@ set -euo pipefail
 # Configure nginx security based on ENV vars.
 #
 # Inputs (aside from all the individual CSP settings):
+# - NGINX_CSP_MODE: defaults to 'enforce'
 # - NGINX_CSP_REPORT_URI: defaults to ''
 # - NGINX_FRAME_OPTIONS: defaults to 'deny', note that setting to `disable` removes the header completely.
 
 # Set defaults
 NGINX_CONFIG_FILE='/etc/nginx/snippets/vars/csp-and-robots.conf'
 NGINX_CSP_ITEMS='child-src connect-src font-src form-action frame-ancestors frame-src img-src manifest-src media-src object-src require-trusted-types-for script-src style-src trusted-types worker-src'
+NGINX_CSP_MODE="${NGINX_CSP_MODE:-enforce}"
 NGINX_CSP_REPORT_URI="${NGINX_CSP_REPORT_URI:-}"
 NGINX_FRAME_OPTIONS="${NGINX_FRAME_OPTIONS:-deny}"
+
+# Validate input
+if [ "${NGINX_CSP_MODE}" = 'enforce' ]; then
+  NGINX_CSP_VAR_NAME='content_security_policy'
+elif [ "${NGINX_CSP_MODE}" = 'report-only' ]; then
+  NGINX_CSP_VAR_NAME='content_security_policy_report_only'
+else
+  echo "Nginx: invalid CSP mode ${NGINX_CSP_MODE}"
+  exit 1
+fi
 
 # Check nginx structure
 if [ ! -f "${NGINX_CONFIG_FILE}" ]; then
@@ -60,15 +72,20 @@ csp_item() {
 nginx_csp_definition() {
   name="$1"
 
-  nginx_var_definition "$name" "$(
-    printf "default-src 'self';"
-    for item in $NGINX_CSP_ITEMS; do
-      csp_item "$item"
-    done
-    csp_item 'report-uri' "$NGINX_CSP_REPORT_URI"
-  )"
+  if [ "$name" = "$NGINX_CSP_VAR_NAME" ]; then
+    nginx_var_definition "$name" "$(
+      printf "default-src 'self';"
+      for item in $NGINX_CSP_ITEMS; do
+        csp_item "$item"
+      done
+      csp_item 'report-uri' "$NGINX_CSP_REPORT_URI"
+    )"
+  else
+    nginx_var_definition "$name" ''
+  fi
 }
 
 # nginx content policy
 echo "Nginx: configuring content security policy…"
 nginx_csp_definition content_security_policy >> "${NGINX_CONFIG_FILE}"
+nginx_csp_definition content_security_policy_report_only >> "${NGINX_CONFIG_FILE}"
