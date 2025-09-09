4a9999c4babc50019526a7169118f04be4c53dc3
David Jennes <david.jennes@wisemen.digital>
2025-09-09 16:23:48 +0200

    Add payload image

---
+++ payload-base/scripts/startup/50-move-static-files.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+# Move static files, so we can serve `/_next/static/…`
+if [ -d "/app/www/.next/static" ]; then
+  if [ -d "/app/www/public" ]; then
+    # Nginx root will be /app/www/public if it exists
+    mv /app/www/.next /app/www/public/_next
+  else
+    # Otherwise it'll just be /app/www
+    mv /app/www/.next /app/www/_next
+  fi
+fi
