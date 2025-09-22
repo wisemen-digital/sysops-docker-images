2b4268039220c395b5f57edca1359a343a3fb66f
David Jennes <david.jennes@wisemen.digital>
2025-09-22 06:14:56 +0200

    CI: test images (using goss) before publishing

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,14 +3,16 @@
 set -euo pipefail
 shopt -s nullglob
 
+# Note: drop the "tag" from the name
 IMAGE_NAME=$1
+SUITE_NAME=${IMAGE_NAME%%:*}
 IMAGE_TARGET=$2
 
 run_test() {
   echo "Running ${1}…"
   pushd "$1" > /dev/null
 
-  tempdir=$(mktemp -d /tmp/${IMAGE_NAME}.XXXXXX)
+  tempdir=$(mktemp -d /tmp/${SUITE_NAME}.XXXXXX)
   [ -d ../../common-files ] && cp -r ../../common-files/* "$tempdir"
   [ -d files ] && cp -rf files/* "$tempdir"
 
@@ -47,6 +49,13 @@ dgoss_run() {
     2> >(grep -v -e '^INFO: ' >&2)
 }
 
-for test in tests/$IMAGE_NAME/$IMAGE_TARGET/*; do
+# Check we have a test suite
+TEST_DIR="tests/$SUITE_NAME/$IMAGE_TARGET"
+if [ ! -d "$TEST_DIR" ]; then
+  echo "Error: missing test suite '$TEST_DIR'"
+  exit 1
+fi
+
+for test in $TEST_DIR/*; do
   run_test "$test"
 done
